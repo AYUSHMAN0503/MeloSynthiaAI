5c64dc70943dd0fc21bc719233d56b12c3db8c7f
PiyushXmishra <piyushbhp83@gmail.com>
2023-08-07 19:40:49 +0530

    Adding footer

--- CLIENT/src/Components/navbar/Hero.tsx
+++ CLIENT/src/Components/navbar/Hero.tsx
@@ -1,6 +1,6 @@
 import styles from "@/index";
 import  music  from "@/assets/music.png";
-
+import { LinearGradient } from 'react-text-gradients'
 
 const Hero = () => {
    
@@ -10,16 +10,18 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
          
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">AI MUSIC </span> {" "}
-            <span className="text-white"> GENERATOR </span> {" "}
-            <span className="text-white">FOR CREATORS</span> 
-          </p>
+          <span className="text-white" style={{ fontSize: "24px" }}> AI MUSIC </span>{" "}
+          <span className="text-white" style={{ fontSize: "24px" }}> GENERATOR </span>{" "}
+          <span className="text-white" style={{ fontSize: "24px" }}> FOR </span>{" "}
+          <span className="text-white" style={{ fontSize: "24px" }}> CREATORS </span>{" "}
+           
+           </p>
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+            <span> <LinearGradient gradient={['to right', '#00FFFF ,#ffffff']}>Generation</LinearGradient></span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
           
@@ -37,11 +39,7 @@ const Hero = () => {
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={music} alt="billing" className="w-[80%] h-[100%] relative z-[5]" style={{backgroundImage:""}}/>
 
-        {/* gradient start */}
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-        {/* gradient end */}
+       
       </div>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
--- CLIENT/src/index.tsx
+++ CLIENT/src/index.tsx
@@ -1,3 +1,9 @@
+import instagram from "@/assets/instagram.svg"
+import twitter from "@/assets/twitter.svg"
+import facebook from "@/assets/facebook.svg"
+import linkedin from "@/assets/linkedin.svg"
+
+
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
   
@@ -27,3 +33,92 @@ const styles = {
   };
   
   export default styles;
+
+  export const footerLinks = [
+    {
+      title: "Lorem ipsum",
+      links: [
+        {
+          name: "Lorem",
+          link: "",
+        },
+        {
+          name: "Lorem",
+          link: "",
+        },
+        {
+          name: "Lorem",
+          link: "",
+        },
+        {
+          name: "Lorem",
+          link: "",
+        },
+        {
+          name: "Lorem",
+          link: "",
+        },
+      ],
+    },
+    {
+      title: "Lorem ipsum",
+      links: [
+        {
+          name: "lorem",
+          link: "",
+        },
+        {
+          name: "lorem",
+          link: "",
+        },
+        {
+          name: "lorem",
+          link: "",
+        },
+        {
+          name: "lorem",
+          link: "",
+        },
+        {
+          name: "lorem",
+          link: "",
+        },
+      ],
+    },
+    {
+      title: "lorem ipsum",
+      links: [
+        {
+          name: "lorem",
+          link: "",
+        },
+        {
+          name: "lorem",
+          link: "",
+        },
+      ],
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: "social-media-1",
+      icon: instagram,
+      link: "https://www.instagram.com/",
+    },
+    {
+      id: "social-media-2",
+      icon: facebook,
+      link: "https://www.facebook.com/",
+    },
+    {
+      id: "social-media-3",
+      icon: twitter,
+      link: "https://www.twitter.com/",
+    },
+    {
+      id: "social-media-4",
+      icon: linkedin,
+      link: "https://www.linkedin.com/",
+    },
+  ];
