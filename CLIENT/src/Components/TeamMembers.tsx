8e8f18dbe2961c7b9ffdaaef483b9f21bab2b71a
Ayushman Mohanty <ayushmanmohanty0503@gmail.com>
2023-08-09 23:26:43 +0530

    github insta icons added and buttons have been activated too

--- CLIENT/src/Components/TeamMembers.tsx
+++ CLIENT/src/Components/TeamMembers.tsx
@@ -4,6 +4,8 @@ const TeamMembers = [
       position: 'Team Leader, Full-Stack Developer and DevOps',
       bio: 'up the seas up the skies',
       imageUrl: '/images/ayushman-mohanty.jpg', 
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
@@ -11,62 +13,80 @@ const TeamMembers = [
       position: 'NFT Developer',
       bio: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique quia nisi aut dicta mollitia magni voluptatibus earum aliquam laudantium at provident est minima, maxime quae debitis ipsum vero beatae vitae explicabo neque assumenda ex harum aperiam doloribus. Maiores, fugit! Enim accusamus dignissimos aperiam temporibus consequuntur.',
       imageUrl: '/images/yash-bansal.jpg', 
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
       name: 'Piyush Kumar Mishra',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/piyush-mishra.jpg',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
       name: 'Aayushya Kumar Chaudhary',
       position: 'Full-stack Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
       name: 'Gaurav Kumar',
       position: 'AI&ML Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
       name: 'Nitin Rana',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
       name: 'Avinash Kumar Jha',
       position: 'Backend and Database Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
       name: 'Mishi Jain',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
-    
+
     {
       name: 'Nikita',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 
     {
       name: 'Dibianchu',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg',
+      imageUrl: '/images/',
+      github: 'https://github.com/johndoe',
+      instagram: 'https://www.instagram.com/johndoe/',
     },
 ];
 
--- CLIENT/src/Components/navbar/AboutUs.tsx
+++ CLIENT/src/Components/navbar/AboutUs.tsx
@@ -1,70 +1,102 @@
 import React, { useState } from 'react';
+import { FaGithub, FaInstagram } from 'react-icons/fa'; // Import icons from the react-icons library
 import TeamMembers from '../TeamMembers';
 
 const AboutUs: React.FC = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-  
-    const handleNextClick = () => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % TeamMembers.length);
-    };
-  
-    const handlePrevClick = () => {
-      setActiveIndex((prevIndex) => (prevIndex - 1 + TeamMembers.length) % TeamMembers.length);
-    };
-  
-    return (
-        <section className="bg-gray-800 py-12"> {/* Change the background color here */}
+  const [activeIndex, setActiveIndex] = useState(0);
 
-        <div className="container mx-auto">
-          <div className="text-white text-center mb-8">
-            <h2 className="text-4xl font-bold">Meet Our Team</h2>
-            <p className="mt-2">A talented group of individuals who are building awesome things.</p>
-          </div>
-  
-          <div className="flex items-center justify-center">
-            <button
-              onClick={handlePrevClick}
-              className="mr-4 text-white hover:text-gray-300 text-3xl transition-transform transform hover:-translate-x-2"
-            >
-              &lt;
-            </button>
-  
-            {/* Display Team Member Card */}
-            <div
-              className="bg-white p-6 rounded-lg shadow-md w-96 transition-transform transform hover:scale-105 hover:-translate-x-2"
-            >
-              <img
-                src={TeamMembers[activeIndex].imageUrl}
-                alt={TeamMembers[activeIndex].name}
-                className="mx-auto w-42 h-42 rounded-full mb-4"
-              />
-              <h3 className="text-xl font-semibold">{TeamMembers[activeIndex].name}</h3>
-              <p className="text-gray-600 mb-4">{TeamMembers[activeIndex].position}</p>
-              <p className="text-gray-800">{TeamMembers[activeIndex].bio}</p>
-            </div>
-  
-            <button
-              onClick={handleNextClick}
-              className="ml-4 text-white hover:text-gray-300 text-3xl transition-transform transform hover:translate-x-2"
-            >
-              &gt;
-            </button>
+  const handleNextClick = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % TeamMembers.length);
+  };
+
+  const handlePrevClick = () => {
+    setActiveIndex((prevIndex) => (prevIndex - 1 + TeamMembers.length) % TeamMembers.length);
+  };
+
+  const handleDotClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <section className="bg-gray-800 py-12">
+      <div className="container mx-auto">
+        <div className="text-white text-center mb-8">
+          <h2 className="text-4xl font-bold">Meet Our Team - WEB 3 Sailors</h2>
+          <p className="mt-2">A talented group of individuals who are behind this concept of MeloSynthia AI.</p>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <button
+            onClick={handlePrevClick}
+            className="mr-4 text-white hover:text-gray-300 text-3xl transition-transform transform hover:-translate-x-2"
+          >
+            &lt;
+          </button>
+
+          {/*Team - Member Card */}
+          <div
+            className="bg-white p-6 rounded-lg shadow-md w-96 transition-transform transform hover:scale-105 hover:-translate-x-2"
+          >
+            <img
+              src={TeamMembers[activeIndex].imageUrl}
+              alt={TeamMembers[activeIndex].name}
+              className="mx-auto w-42 h-42 rounded-full mb-4"
+            />
+            <h3 className="text-xl font-semibold flex items-center">
+              {TeamMembers[activeIndex].name}
+
+              {/* GitHub Icon */}
+              {TeamMembers[activeIndex].github && (
+                <a
+                  href={TeamMembers[activeIndex].github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-2 text-gray-600 hover:text-gray-800"
+                >
+                  <FaGithub />
+                </a>
+              )}
+              
+              {/* Instagram Icon */}
+              {TeamMembers[activeIndex].instagram && (
+                <a
+                  href={TeamMembers[activeIndex].instagram}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-2 text-gray-600 hover:text-gray-800"
+                >
+                  <FaInstagram />
+                </a>
+              )}
+            </h3>
+            <p className="text-gray-600 mb-4">{TeamMembers[activeIndex].position}</p>
+            <p className="text-gray-800">{TeamMembers[activeIndex].bio}</p>
           </div>
 
-          {/* Navigation Dots */}
+          <button
+            onClick={handleNextClick}
+            className="ml-4 text-white hover:text-gray-300 text-3xl transition-transform transform hover:translate-x-2"
+          >
+            &gt;
+          </button>
+        </div>
+
+        {/*Dots*/}
         <div className="flex justify-center mt-4">
           {TeamMembers.map((_, index) => (
             <div
               key={index}
-              className={`w-2 h-2 mx-1 rounded-full ${
+              onClick={() => handleDotClick(index)}
+              className={`w-2 h-2 mx-1 rounded-full cursor-pointer ${
                 index === activeIndex ? 'bg-gray-500' : 'bg-gray-200'
               }`}
             />
           ))}
         </div>
-        </div>
-      </section>
-    );
-  };
-  
-  export default AboutUs;
+      </div>
+    </section>
+
+  );
+};
+
+export default AboutUs;
