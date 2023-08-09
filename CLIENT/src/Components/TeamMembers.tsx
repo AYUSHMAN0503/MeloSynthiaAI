ffbc4c2f54e1a0ad22b1f61ef7dd8f6386703d26
Ayushman Mohanty <ayushmanmohanty0503@gmail.com>
2023-08-09 22:29:29 +0530

    about -us modified

--- CLIENT/src/Components/TeamMembers.tsx
+++ CLIENT/src/Components/TeamMembers.tsx
@@ -3,31 +3,31 @@ const TeamMembers = [
       name: 'Ayushman Mohanty',
       position: 'Team Leader, Full-Stack Developer and DevOps',
       bio: 'up the seas up the skies',
-      imageUrl: '/images/ayushman-mohanty.jpg', // Adjust the image URL
+      imageUrl: '/images/ayushman-mohanty.jpg', 
     },
     {
       name: 'Bash Yansal',
       position: 'Backend Developer',
       bio: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique quia nisi aut dicta mollitia magni voluptatibus earum aliquam laudantium at provident est minima, maxime quae debitis ipsum vero beatae vitae explicabo neque assumenda ex harum aperiam doloribus. Maiores, fugit! Enim accusamus dignissimos aperiam temporibus consequuntur.',
-      imageUrl: '/images/yash-bansal.jpg', // Adjust the image URL
+      imageUrl: '/images/yash-bansal.jpg', 
     },
     {
       name: 'John Doe',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg', // Adjust the image URL
+      imageUrl: '/images/john-doe.jpg',
     },
     {
       name: 'John Doe',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg', // Adjust the image URL
+      imageUrl: '/images/john-doe.jpg',
     },
     {
       name: 'John Doe',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
-      imageUrl: '/images/john-doe.jpg', // Adjust the image URL
+      imageUrl: '/images/john-doe.jpg',
     },
     
     
