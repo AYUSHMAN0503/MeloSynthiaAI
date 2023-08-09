d32977c7af2e552a76a3ef7f1187f852432b52cd
Ayushman Mohanty <ayushmanmohanty0503@gmail.com>
2023-08-09 22:37:13 +0530

    updated about us and team members

--- CLIENT/src/Components/TeamMembers.tsx
+++ CLIENT/src/Components/TeamMembers.tsx
@@ -5,33 +5,70 @@ const TeamMembers = [
       bio: 'up the seas up the skies',
       imageUrl: '/images/ayushman-mohanty.jpg', 
     },
+
     {
-      name: 'Bash Yansal',
-      position: 'Backend Developer',
+      name: 'Yash Bansal',
+      position: 'NFT Developer',
       bio: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique quia nisi aut dicta mollitia magni voluptatibus earum aliquam laudantium at provident est minima, maxime quae debitis ipsum vero beatae vitae explicabo neque assumenda ex harum aperiam doloribus. Maiores, fugit! Enim accusamus dignissimos aperiam temporibus consequuntur.',
       imageUrl: '/images/yash-bansal.jpg', 
     },
+
     {
-      name: 'John Doe',
+      name: 'Piyush Kumar Mishra',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
       imageUrl: '/images/john-doe.jpg',
     },
+
     {
-      name: 'John Doe',
+      name: 'Aayushya Kumar Chaudhary',
+      position: 'Full-stack Developer',
+      bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
+      imageUrl: '/images/john-doe.jpg',
+    },
+
+    {
+      name: 'Gaurav Kumar',
+      position: 'AI&ML Developer',
+      bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
+      imageUrl: '/images/john-doe.jpg',
+    },
+
+    {
+      name: 'Nitin Rana',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
       imageUrl: '/images/john-doe.jpg',
     },
+
     {
-      name: 'John Doe',
+      name: 'Avinash Kumar Jha',
+      position: 'Backend and Database Developer',
+      bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
+      imageUrl: '/images/john-doe.jpg',
+    },
+
+    {
+      name: 'Mishi Jain',
       position: 'Frontend Developer',
       bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
       imageUrl: '/images/john-doe.jpg',
     },
     
-    
-  ];
+    {
+      name: 'Nikita',
+      position: 'Frontend Developer',
+      bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
+      imageUrl: '/images/john-doe.jpg',
+    },
+
+    {
+      name: 'Dibianchu',
+      position: 'Frontend Developer',
+      bio: 'Passionate about crafting user-friendly and visually appealing web experiences.',
+      imageUrl: '/images/john-doe.jpg',
+    },
+];
 
 {/*images used here are taken from public/images*/}
   
