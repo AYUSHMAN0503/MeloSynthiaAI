81eca9b56e9d53a9167e49328097de790aecef3e
Ayushman Mohanty <ayushmanmohanty0503@gmail.com>
2023-08-12 15:45:42 +0530

    features section structure added

--- CLIENT/src/Components/FeaturesSection.tsx
+++ CLIENT/src/Components/FeaturesSection.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 
 const FeaturesSection: React.FC = () => {
   return (
-    <section className="bg-gray-200 py-10">
+    <section className="bg-app-bg py-10">
       <div className="container mx-auto px-4">
-        <div className="flex flex-wrap items-center">
-          {/* Image on the left (Desktop) */}
-          <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <img src="/path/to/your/image.png" alt="Feature" className="w-full" />
-          </div>
+        <div className="text-center mb-6">
+          <h2 className="text-5xl font-semibold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-500">
+            MeloSynthia AI features
+          </h2>
+          <p className="text-2xl font-semibold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-500">MeloSynthia AI where creativity meets creation..</p>
+        </div>
 
-          {/* Features description (Desktop) */}
-          <div className="w-full md:w-1/2 md:pl-10">
-            <h2 className="text-3xl font-semibold mb-4">Features</h2>
-            <p className="text-gray-600 mb-6">Your tagline or subheader here</p>
-
+        <div className="flex flex-col md:flex-row items-center justify-center md:justify-start">
+          {/* Features description (Centered) */}
+          <div className="w-full md:w-1/2 md:pr-10 text-center md:text-left">
             <div className="flex flex-col space-y-6">
               {/* Feature Box 1 */}
-              <div className="flex items-center space-x-4">
+              <div className="flex items-center space-x-4 border border-white rounded-lg p-4">
                 <div className="bg-blue-500 rounded-full p-3">
                   {/* Icon */}
                   <svg
@@ -27,17 +26,16 @@ const FeaturesSection: React.FC = () => {
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                   >
-                    {/* Your icon SVG code here */}
+                    {/* icon SVG code here */}
                   </svg>
                 </div>
                 <div>
-                  <h3 className="text-xl font-semibold">Feature 1</h3>
-                  <p className="text-gray-600">Description of Feature 1</p>
+                  <h3 className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-500">Marketplace</h3>
+                  <p className="text-gray-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ullam officiis, nihil nam molestiae optio doloribus ex ducimus culpa, ipsam modi magni?</p>
                 </div>
               </div>
-
-              {/* Feature Box 2 */}
-              <div className="flex items-center space-x-4">
+              {/* ... Feature Box 2 ... */}
+              <div className="flex items-center space-x-4 border border-white rounded-lg p-4">
                 <div className="bg-blue-500 rounded-full p-3">
                   {/* Icon */}
                   <svg
@@ -47,17 +45,16 @@ const FeaturesSection: React.FC = () => {
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                   >
-                    {/* Your icon SVG code here */}
+                    {/* icon SVG code here */}
                   </svg>
                 </div>
                 <div>
-                  <h3 className="text-xl font-semibold">Feature 1</h3>
-                  <p className="text-gray-600">Description of Feature 1</p>
+                  <h3 className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-500">Highest quality audio</h3>
+                  <p className="text-gray-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam aut nisi, in asperiores reprehenderit tempore enim iste assumenda commodi earum!</p>
                 </div>
               </div>
-              
-              {/* Feature Box 3 */}
-              <div className="flex items-center space-x-4">
+              {/* ... Feature Box 3 ... */}
+              <div className="flex items-center space-x-4 border border-white rounded-lg p-4">
                 <div className="bg-blue-500 rounded-full p-3">
                   {/* Icon */}
                   <svg
@@ -67,15 +64,20 @@ const FeaturesSection: React.FC = () => {
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                   >
-                    {/* Your icon SVG code here */}
+                    {/*icon SVG code here */}
                   </svg>
                 </div>
                 <div>
-                  <h3 className="text-xl font-semibold">Feature 1</h3>
-                  <p className="text-gray-600">Description of Feature 1</p>
+                  <h3 className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-500">Song Snippets</h3>
+                  <p className="text-gray-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, explicabo maiores unde quisquam cumque sit mollitia deleniti beatae possimus nihil?</p>
                 </div>
               </div>
             </div>
+          </div>
+
+          {/* Image on the left (Desktop) */}
+          <div className="w-full md:w-1/2">
+            <img src='images/tunes.jpg' alt="Feature" className="w-full h-auto" />
           </div>
         </div>
       </div>
--- CLIENT/src/Steps/ShowSteps.tsx
+++ CLIENT/src/Steps/ShowSteps.tsx
@@ -24,28 +24,26 @@ const parentVariants = {
       opacity: 1,
     },
   }
-  //end
 
 function ShowSteps() {
   return (
     <>
       <section className='py-16 p-4'>
-        <h1 className='text-3xl pb-5 bg-clip-text text-cyan-400 text-center text-transparent '>STEPS</h1>
+        <h1 className='text-6xl pb-5 bg-clip-text text-cyan-400 text-center text-transparent '>STEPS</h1>
         <div className='container mx-auto max-w-6xl'>
-          {/* Grid */}
           <motion.div
             variants={parentVariants}
             initial='hidden'
             whileInView='show'
             viewport={{ once: true }}
             className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-white gap-6'
-            >
-            
+          >
             <motion.div variants={childVariants}>
               <Steps
                 title='Connect your wallet'
                 desc='Use Trust Wallet, Metamask or any wallet to connect to the app.'
                 icon='1'
+                textSize='text-xl'
               />
             </motion.div>
             <motion.div variants={childVariants}>
@@ -53,6 +51,7 @@ function ShowSteps() {
                 title='Create your NFT Item'
                 desc='Upload your NFTs and set a title, description and price.'
                 icon='2'
+                textSize='text-xl'
               />
             </motion.div>
             <motion.div variants={childVariants}>
@@ -60,13 +59,14 @@ function ShowSteps() {
                 title='List them for sale'
                 desc='Earn ETH and BIT for all your NFTs that you sell on our marketplace.'
                 icon='3'
+                textSize='text-xl'
               />
             </motion.div>
           </motion.div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
-export default ShowSteps
+export default ShowSteps;
