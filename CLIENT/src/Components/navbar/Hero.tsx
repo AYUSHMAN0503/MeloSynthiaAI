7b920f55845bf52471294472fc31f7c63a151251
Nitin <nitinrana01125532553@gmail.com>
2023-08-13 01:59:31 +0530

    added some padding in hero

--- CLIENT/src/Components/navbar/Hero.tsx
+++ CLIENT/src/Components/navbar/Hero.tsx
@@ -8,7 +8,7 @@ import "./Test3.css"
 const Hero = () => {
 
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} px-6`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
 
