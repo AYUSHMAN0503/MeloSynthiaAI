d08bd3b948421757df17683eb323eb3e8b4228bc
PiyushXmishra <piyushbhp83@gmail.com>
2023-08-13 12:07:26 +0530

    reverting changes of icon

--- CLIENT/src/Components/navbar/MainContent.tsx
+++ CLIENT/src/Components/navbar/MainContent.tsx
@@ -6,7 +6,7 @@ const MainContent: React.FC = () => {
   return (
     <main className="p-7 mt-6">
       <div className="max-w-4xl mx-auto">
-      <p className="flex justify-center text-3xl ss:text-[px] "> <LinearGradient gradient={['to left', '#17acff ,#ff68f0']}>   Discover the power of AI led music generation. </LinearGradient></p>
+      <p className="flex justify-center text-3xl text-[px]  "> <LinearGradient gradient={['to left', '#17acff ,#ff68f0']}>   Discover the power of AI led music generation. </LinearGradient></p>
       </div>
     </main>
   );
