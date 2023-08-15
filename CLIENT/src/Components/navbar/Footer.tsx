9c7b163aa32fa0aaaa3b961e0ecc178bdfe8287d
Nitin <nitinrana01125532553@gmail.com>
2023-08-15 21:28:11 +0530

    improved footer links

--- CLIENT/src/Components/navbar/Footer.tsx
+++ CLIENT/src/Components/navbar/Footer.tsx
@@ -2,16 +2,23 @@ import styles from "@/index";
 import logo from "@/assets/melosynthia-ai-high-resolution-logo-color-on-transparent-background.png";
 import { socialMedia} from "@/index";
 import { Link } from "react-router-dom";
+import React from "react";
 
 const scrollToTop = () => {
   window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
 };
 
-const Footer = () => (
-  
+
+
+const Footer = () => {
+
+const [about, setAbout] = React.useState(false);
+
+const [terms, setTerms] = React.useState(false); 
+return (
   <>
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col !pb-5`}>
-      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col w-full mb-0 sm:mb-14`}>
         <div className="flex-[1] flex flex-col justify-start mr-10">
           <img
             src={logo}
@@ -24,42 +31,45 @@ const Footer = () => (
         <div className="py-3" >
           <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
             <div className="md:flex md:justify-between">
-              <div className="mb-6 md:mb-0">
-
-              </div>
-              <div className="font-poppins font-normal grid grid-cols-2 gap-8 sm:gap-28 sm:grid-cols-3" onClick={scrollToTop}>
+              <div className="font-poppins font-normal sm:grid grid-cols-2 gap-8 sm:gap-48" >
                 <div>
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium" >
+                  <button type="button" className="text-white font-bold font-poppins mb-4" onClick={(e) => {
+                    e.preventDefault();
+                      setAbout(prev => !prev);
+                  }}>
+                    About and Beyond
+                  </button>
+                <ul className={about ? 'text-gray-500 dark:text-gray-400 font-medium ' : 'hidden'} onClick={scrollToTop}>
                     <li className="mb-4" >
-                      <Link to="/AboutUs" className="hover:underline">About Us</Link>
+                      <Link to="/AboutUs" className="hover:underline">About </Link>
                     </li>
-                    <li>
-                      <Link to="/Future" className="hover:underline">Future</Link>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
                     <li className="mb-4">
-                      <Link to="#" className="hover:underline ">Lorem</Link>
+                      <Link  to="/Future" className="hover:underline">Future</Link>
                     </li>
-                    <li>
-                      <Link to="#" className="hover:underline">Lorem</Link>
+                    <li >
+                      <Link  to="/Future" className="hover:underline">Lorem</Link>
                     </li>
-                  </ul>
-                </div>
-                <div>
-
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                    </ul>
+                  </div>
+                  <div className="mt-6 sm:mt-0">
+                  <button className="text-white font-bold font-poppins mb-4" onClick={(e) => {
+                      e.preventDefault();
+                      setTerms(prev => !prev);
+                    }}>
+                    Terms Of Use
+                  </button>
+                  <ul className={terms ? 'text-gray-500 dark:text-gray-400 font-medium' : 'hidden'} onClick={scrollToTop}>
                     <li className="mb-4">
                       <Link to="/TermsOfService" className="hover:underline">Terms Of Service</Link>
                     </li>
-                    <li>
+                    <li className="mb-4">
                       <Link to="/PrivacyPolicy" className="hover:underline">Privacy Policy</Link>
                     </li>
-                  </ul>
-                </div>
+                    <li>
+                      <Link to="/PrivacyPolicy" className="hover:underline">Lorem</Link>
+                    </li>
+                    </ul>
+                  </div>
               </div>
             </div>
           </div>
@@ -85,7 +95,7 @@ const Footer = () => (
         </div>
       </div>
     </section></>
-);
+)};
 
 export default Footer;
 
