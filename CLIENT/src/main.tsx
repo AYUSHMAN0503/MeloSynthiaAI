9991b102e34f8e19566821d4133cd6a7a7478f32
PiyushXmishra <piyushbhp83@gmail.com>
2023-08-19 23:29:23 +0530

    Fixing issue of theme after adding Chakra UI

--- CLIENT/src/main.tsx
+++ CLIENT/src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ChakraProvider } from "@chakra-ui/react";
-
+import { ChakraProvider, background } from "@chakra-ui/react";
+import theme from '@material-tailwind/react/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <App />
     </ChakraProvider>
   </React.StrictMode>,
