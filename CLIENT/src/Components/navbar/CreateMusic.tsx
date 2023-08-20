cc0a42287dd0404eff2db0d8f3f0a44f83916e12
PiyushXmishra <piyushbhp83@gmail.com>
2023-08-20 09:39:59 +0530

    Adding Responsiveness in song snippets section and changing some sections

--- CLIENT/src/Components/navbar/CreateMusic.tsx
+++ CLIENT/src/Components/navbar/CreateMusic.tsx
@@ -13,61 +13,62 @@ import {
   TabPanel,
 } from "@material-tailwind/react";
 import CardList from './Cardlist';
- import { CardList2 } from './Cardlist';
+import { CardList2 } from './Cardlist';
+import CustomFileInput from './FileInput';
 const cardsData = [
   {
     title: 'Hip-Hop',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1546528377-65924be33e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1546528377-65924be33e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
   },
   {
     title: 'Classic',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1558584673-c834fb1cc3ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1558584673-c834fb1cc3ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80',
   },
   {
     title: 'Jazz',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1415201364774-f6f0bb35f28f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1415201364774-f6f0bb35f28f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   },
   {
     title: 'Cultural',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1568219656418-15c329312bf1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1568219656418-15c329312bf1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   },
   {
     title: 'Melody',
     description: 'Description for Card 1',
-    imageUrl: 'https://plus.unsplash.com/premium_photo-1681593282801-5e6ae37eac2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',   
+    imageUrl: 'https://plus.unsplash.com/premium_photo-1681593282801-5e6ae37eac2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
   },
   {
     title: 'Symphony',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1465847899084-d164df4dedc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1465847899084-d164df4dedc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   },
- 
+
 ];
 const cardsData2 = [
   {
     title: 'Ambient',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   },
   {
     title: 'LoFi',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1630713815150-2c847025c1d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1630713815150-2c847025c1d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   },
   {
     title: 'Slowed-Reverb',
     description: 'Description for Card 1',
-    imageUrl: 'https://images.unsplash.com/photo-1631044176346-804c33ade61c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG8lMjBmaXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',   
+    imageUrl: 'https://images.unsplash.com/photo-1631044176346-804c33ade61c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG8lMjBmaXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
   },
 
 ];
 const cardsData3 = [
   {
-    title: '00:10' 
+    title: '00:10'
   },
   {
     title: '00:20'
@@ -78,16 +79,16 @@ const cardsData3 = [
   {
     title: '00:40'
   },
- {
+  {
     title: '00:50'
-   },
- {
+  },
+  {
     title: '01:00'
-   },
+  },
 ];
 const cardsData4 = [
   {
-    title: 'Slow'  
+    title: 'Slow'
   },
   {
     title: 'Normal'
@@ -183,29 +184,30 @@ export function TabsDefault() {
               <h3 className='p-10 flex justify-left'>
                 Your Generated NFTs/Music
               </h3>
-            </div> 
-
-              <div className='w-full flex  flex-row justify-center items-center py-8 flex-wrap'>
-                <textarea
-                  className='mr-[4vw] appearance-none bg-transparent w-[40vw] mb-6 text-white-700 py-[1vw] focus:outline-none text-lg border-2 rounded-lg pl-4'
-                  placeholder="A pop music with Eminem song's beat"
-                />
-                <button
-                  className='flex-shrink-0 border-transparent bg-cyan-500 hover:bg-cyan-700 border-4 border-cyan-500 hover:border-cyan-700 text-lg text-white px-[2vw] py-[1vw] rounded-lg'
-                  type='button'>
-                  Compose
-                </button>
-              </div>
             </div>
+
+            <div className='w-full flex  flex-row justify-center items-center py-8 flex-wrap'>
+              <textarea
+                className='mr-[4vw] appearance-none bg-transparent w-[40vw] mb-6 text-white-700 py-[1vw] focus:outline-none text-lg border-2 rounded-lg pl-4'
+                placeholder="A pop music with Eminem song's beat"
+              />
+              <button
+                className='flex-shrink-0 border-transparent bg-cyan-500 hover:bg-cyan-700 border-4 border-cyan-500 hover:border-cyan-700 text-lg text-white px-[2vw] py-[1vw] rounded-lg'
+                type='button'>
+                Compose
+              </button>
+            </div>
+          </div>
 
         </TabPanel>
         <TabPanel value="song-snippets">
           <div className="w-full flex flex-col">
             <div className="text-center text-white px-4 py-8">
-              <h1 className="text-7xl"><LinearGradient gradient={['to left', '#17acff ,#ff68f0']}>Song Snippets </LinearGradient></h1>
+              <h1 className="text-3xl font-semibold sm:text-5xl lg:text-7xl"><LinearGradient gradient={['to left', '#17acff ,#ff68f0']}>Song Snippets </LinearGradient></h1>
             </div>
-            <div className="flex flex-row gap-5 sm:gap-7 w-full justify-center py-4 flex-wrap">
-              <h2 className="text-white text-3xl md:text-4xl">Genre:</h2>
+            <h2 className="text-white text-3xl md:text-4xl">Genre:</h2>
+            <div className="flex-row sm:gap-7 w-full justify-center py-4 flex-wrap grid grid-cols-2 ss:grid-cols-3 sm:grid-cols-4 gap-4  md:grid-cols-6 lg:grid-cols-6">
+
               <button className="ring ring-yellow-400 py-2 px-3 rounded text-white text-lg hover:bg-gray-900">Hip Hop</button>
               <button className="ring ring-yellow-400 py-2 px-3 rounded text-white text-lg hover:bg-gray-900">Rock</button>
               <button className="ring ring-yellow-400 py-2 px-3 rounded text-white text-lg hover:bg-gray-900">Metal</button>
@@ -216,23 +218,15 @@ export function TabsDefault() {
               <button className="ring ring-yellow-400 py-2 px-3 rounded text-white text-lg hover:bg-gray-900">Slow & Reverb</button>
             </div>
             <div className="w-full flex flex-wrap justify-center py-6 px-2 mt-4 gap-6">
-                <label htmlFor="vocalFile" className="w-100 lg:w-1/3 py-2 sm:py-3 px-4 xl:px-6 rounded bg-gradient-to-r from-gold-500 to-purple-500 hover:cursor-pointer">
-                  <div className="flex flex-row gap-4 py-2">
-                    <AddBoxOutlinedIcon className="scale-150" />
-                    <p className="text-xl text-center text-gray-900 mt-[-0.2rem]">Drop your vocal file here</p>
-                  </div>
-                </label>
-              <input type="file" id="vocalFile" className="hidden" />
-              <button
-                className='border-transparent bg-cyan-500 hover:bg-cyan-700 border-4 border-cyan-500 hover:border-cyan-700 text-xl text-white px-4 py-2 rounded-lg'
-                type='button'>
-                Create
+              <CustomFileInput />
+              <button className="w-32 h-12 content-center flex-wrap self-center flex justify-center bg-transparent bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Create
               </button>
             </div>
             <div className="text-white w-full flex justify-center text-lg m-4">
               <p><span className="mr-1 font-bold">Caution:</span> MeloSynthiaAI does not Support and is not responsible for any copyright infringement of the vocals given in NFTs.</p>
             </div>
           </div>
+
         </TabPanel>
       </TabsBody>
     </Tabs>
