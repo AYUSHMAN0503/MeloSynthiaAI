6981bffe85bc97b6f492ae8d5e0903dc83332ae2
Ayushman Mohanty <ayushmanmohanty0503@gmail.com>
2023-08-31 11:11:46 +0530

    marketplace updated a lil bit

--- CLIENT/src/Components/Marketplace.tsx
+++ CLIENT/src/Components/Marketplace.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Tabs, TabList, Tab, TabPanel,TabPanels } from '@chakra-ui/react';
 import BuyNFTs from './BuyNFTs';
 import SellNFTs from './SellNFTs';
+import Animatedpage from './Animatedpage';
 
 const Marketplace= () => {
   return (
-    <div className='pt-8'>
-      <h1 className="text-2xl font-semibold mb-4 text-white">Marketplace</h1>
+    <div className='pt-6'>
+      <h1 className="text-6xl font-bold mt-1 text-center bg-gradient-to-r from-blue-500 to-pink-500 text-transparent bg-clip-text">Marketplace</h1>
+         <Animatedpage>
+            <h4 className="mb-5 mt-1 font-semibold bg-gradient-to-r from-blue-500 to-pink-500 text-transparent bg-clip-text text-center sm:text-1xl md:text-2xl lg:text-3xl">
+                An interactive Marketplace to sell and purchase NFTs..
+            </h4>
+         </Animatedpage>
+      
       <Tabs>
-         <TabList>
+         <TabList className="text-white justify-center text-3xl">
             <Tab>Buy</Tab>
             <Tab>Sell</Tab>
          </TabList>
