4c7e6a00d77cb71d2d1a9cd85c51d7c4603403d1
Divyanshu <divyanshu>
2023-09-13 14:20:00 +0530

    termsofservice changes

--- CLIENT/src/Components/testimonialsData.ts
+++ CLIENT/src/Components/testimonialsData.ts
@@ -1,17 +1,17 @@
 // testimonialsData.ts
 const testimonials = [
     {
-      name: "Alice",
+      name: "Alicia",
       image: "https://randomuser.me/api/portraits/women/84.jpg",
       text: "MeloSynthia is amazing! I was able to create a beautiful song for my video in minutes. It's so easy and fun to use.",
     },
     {
-      name: "Bob",
+      name: "Martin",
       image: "https://randomuser.me/api/portraits/men/2.jpg",
       text: "melosynthia is a game changer! I can customize my music to fit my mood and style. It's like having a personal composer.",
     },
     {
-      name: "Emma",
+      name: "Elizabeth",
       image: "https://randomuser.me/api/portraits/women/10.jpg",
       text: "melosynthia is a worry-free solution! I don't have to worry about copyright issues or paying for expensive music licenses. It's all royalty-free!",
     },
