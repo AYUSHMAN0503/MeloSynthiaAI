4956be4ac7d7eb11988c18317a50c80fe91309b5
Ayushman Mohanty <ayushmanmohanty0503@gmail.com>
2023-10-05 20:12:19 +0530

    vercel ready

--- CLIENT/src/theme.tsx
+++ CLIENT/src/theme.tsx
@@ -1,13 +1,13 @@
 import { extendTheme } from "@chakra-ui/react";
-import type { StyleFunctionProps } from "@chakra-ui/styled-system";
 
 const theme = extendTheme({
   styles: {
-    global: (props: StyleFunctionProps) => ({
+    global: {
       body: {
-        bg:"linear-gradient(315deg, #130f40 0%, #000000 74%"
-      }
-    })
+        bg: "linear-gradient(315deg, #130f40 0%, #000000 74%)",
+      },
+    },
   },
-})
-export default theme
+});
+
+export default theme;
